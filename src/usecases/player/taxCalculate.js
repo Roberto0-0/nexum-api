705cf406505badce4aeeb371c95ceeaf147d9fc9
Robert <roberto88526@gmail.com>
2025-05-12 19:10:36 -0300

    feat: Salary updated.
    Salaries adjusted according to jobs.

--- src/usecases/player/taxCalculate.js
+++ src/usecases/player/taxCalculate.js
@@ -4,10 +4,10 @@ class PlayerTaxCalculate {
 
         const tax = [
             { minimumSalary: 600, maximumSalary: 2670, percentage: 0 },
-            { minimumSalary: 2641, maximumSalary: 4000, percentage: 7.5 },
-            { minimumSalary: 4001, maximumSalary: 6750, percentage: 15 },
-            { minimumSalary: 6751, maximumSalary: 10000, percentage: 22.5 },
-            { minimumSalary: 10000, maximumSalary: 50000, percentage: 27.5 }
+            { minimumSalary: 2641, maximumSalary: 7000, percentage: 7.5 },
+            { minimumSalary: 7001, maximumSalary: 15000, percentage: 15 },
+            { minimumSalary: 15001, maximumSalary: 20000, percentage: 22.5 },
+            { minimumSalary: 20001, maximumSalary: 50000, percentage: 27.5 }
         ]
 
         for (let i = 0; i < tax.length; i++) {
